c3cf54e368da58e31f5c44a609cb9696a755a6a7
nt636@tux.cci.drexel.edu <khanhnguyentrinhthi@gmail.com>
2025-09-06 09:58:31 -0400

    Remove unused libraries

--- external/linbox/linboxbuild/fflas-ffpack-2.5.0/autotune/tune_fsytrf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo =================================================
-echo ========= FFLAS-FFPACK fsytrf Autotuning ========
-echo =================================================
-echo 
-(./fsytrf > fsytrf-threshold.h) 2>&1 | tee fsytrf-autotune.log
-val=${PIPESTATUS[0]}; if test ${val} -ne 0 ; then exit ${val}; fi
