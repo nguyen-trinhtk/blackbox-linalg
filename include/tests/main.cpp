80acbb8854cd6a1837d02b0c2e7974434a980849
nt636@tux.cci.drexel.edu <khanhnguyentrinhthi@gmail.com>
2025-09-08 14:27:53 -0400

    Fix make

--- include/tests/main.cpp
+++ include/tests/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main() {
     cout << "Ready!" << endl;
-    cout << "Run one of the below tests:" << endl;
+    cout << "Run one of the below:" << endl;
     cout << "- run-matvec" << endl;
     cout << "- run-bm" << endl;
     cout << "- run-wiedemann" << endl;
